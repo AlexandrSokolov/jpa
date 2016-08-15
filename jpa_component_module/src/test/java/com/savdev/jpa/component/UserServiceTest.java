e3dc8530495c4924e5945f44fd15082741a27683
alexandr <alexandrsokolov@users.noreply.github.com>
2016-08-15 19:13:50 +0200

    Added integration test with Arquillian Added TODO list for parent module

--- jpa_component_module/src/test/java/com/savdev/jpa/component/UserServiceTest.java
+++ jpa_component_module/src/test/java/com/savdev/jpa/component/UserServiceTest.java
@@ -1,8 +1,49 @@
 package com.savdev.jpa.component;
 
+import java.io.File;
+import java.util.ResourceBundle;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 /**
  *
  */
+@RunWith(Arquillian.class)
 public class UserServiceTest {
+
+    @Inject
+    UserService userService;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("tests");
+        String baseDir = resourceBundle.getString("basedir");
+        File[] files = Maven.resolver().loadPomFromFile(baseDir + File.separator + "pom.xml")
+                .importDependencies(ScopeType.COMPILE, ScopeType.TEST, ScopeType.PROVIDED, ScopeType.RUNTIME, ScopeType.IMPORT).resolve().withTransitivity().asFile();
+
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "user_test.war")
+                .addClasses(UserService.class)
+                .addAsLibraries(files)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+        System.out.println(war.toString(true));
+
+        return war;
+    }
+
+    @Test
+    public void should_create_greeting() {
+        Assert.assertEquals("test", "test");
+    }
 
 }
